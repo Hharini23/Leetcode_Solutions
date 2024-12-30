d5512c07101bf8600b25564f7875670f3c239094
HARINI S <125809388+Hharini23@users.noreply.github.com>
2024-12-30 19:08:27 +0530

    Time: 2 ms (97.51%), Space: 41.6 MB (81.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String str) {
+        Stack <Character> s = new Stack<>();
+        for(char ch : str.toCharArray())
+        {
+        if(ch=='(')
+        {
+            s.push(')');
+        }
+        else if(ch=='[')
+        {
+            s.push(']');
+        }
+        else if(ch=='{')
+        {
+            s.push('}');
+        }
+        else if(s.isEmpty() || s.pop()!=ch)
+        {
+            return false;
+        }
+        }
+        return s.isEmpty();
+
+    }
+}
