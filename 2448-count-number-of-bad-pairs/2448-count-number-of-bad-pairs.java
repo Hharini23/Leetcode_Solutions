fc0cdf59061865867f802e07e13d052990e70b49
HARINI S <125809388+Hharini23@users.noreply.github.com>
2025-02-11 13:12:16 +0530

    Time: 42 ms (27.4%), Space: 59.3 MB (29.59%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -19,54 +19,21 @@
 
 
 
-// class Solution {
-//     public long countBadPairs(int[] nums) {
-//         int n = nums.length;
-//         int tot=n*(n-1)/2;
-//         long goodpair=0;
-//         HashMap <Integer,Integer> m= new HashMap<>();
-//         for(int i=0;i<n;i++)
-//         {
-//              if(m.containsKey(nums[i]-i))
-//              {
-//                 goodpair+=(long)m.get(nums[i]-i);
-//              }
-//              m.put(nums[i]-i,m.getOrDefault(nums[i]-i,0)+1);
-//         }
-        
-//         return (long)tot-goodpair;
-//     }
-// }
-
 class Solution {
     public long countBadPairs(int[] nums) {
         int n = nums.length;
-        Map<Integer,Integer> mp = new HashMap<>();
-        long cnt = 0;
-        for(int i=0;i<n;i++){
-            if(mp.containsKey(nums[i]-i)){
-                cnt += (long)mp.get(nums[i]-i);
-            }
-            mp.put(nums[i]-i,mp.getOrDefault(nums[i]-i,0)+1);
-
+        long goodpair=0;
+        HashMap <Integer,Integer> m= new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+             if(m.containsKey(nums[i]-i))
+             {
+                goodpair+=(long)m.get(nums[i]-i);
+             }
+             m.put(nums[i]-i,m.getOrDefault(nums[i]-i,0)+1);
         }
-
-        return (long)n*(n-1)/2-cnt;
-
-
         
+        return (long)n*(n-1)/2-goodpair;
     }
-    /*
-    1 2 4
-    1->2
-    4->1
+}
 
-    (1,2),(1,4),(2,4) -> n*(n-1)/2 total pairs
-    j - i = a[j]-a[i]
-
-    a[i] - i = a[j] - j
-
-
-
-    */
-} 
